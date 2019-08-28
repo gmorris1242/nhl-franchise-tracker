37fdd06bce594e3db5bb4f9235e88ad30a19586b
George Morris <gmorris1242@gmail.com>
2019-08-28 00:41:19 -0400

    Start on front-end

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def get_seasons_for_nav()
+    @seasons =  Season.where(franchise_id: params[:franchise_id])
+  end
+
+  def get_team_logo(team)
+    logos = {
+      'Detroit Red Wings' => 17,
+      'Minnesota Wild' => 30,
+      'New York Islanders' => 2
+    }
+
+
+    "https://www-league.nhlstatic.com/images/logos/teams-current-primary-light/#{logos[team]}.svg"
+  end
 end
