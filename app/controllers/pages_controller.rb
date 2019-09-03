4364d1e1e4ae0ff8231dcaf5bca58b3823eb5bea
George Morris <gmorris1242@gmail.com>
2019-09-03 16:40:15 -0400

    Add total records

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
-  def home
+
+  def records
+    redirect_to :root unless current_user
+    @seasons = []
+    franchises  = Franchise.where(user_id: current_user.id)
+    franchises.each do |franchise|
+      franchise.seasons.each do |season|
+        @seasons.push(season)
+      end
+    end
   end
 end
